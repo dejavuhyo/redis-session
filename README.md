74539559ae67a671a99e2527bdff4e9bead1057e
공대베짱이 <dejavuhyo@gmail.com>
2022-04-26 11:41:36 +0900

    Create redis-session

---
+++ README.md
@@ -0,0 +1,24 @@
+# Spring Scheduler
+
+## 1. 설명
+Spring Boot에서 Redis와 연동하여 Session Storage로 사용하는 예제이다.
+
+## 2. 개발환경
+
+* OpenJDK 11
+
+* spring-boot 2.6.7
+
+## 3. 실행
+
+### 1) Session 생성
+
+* http://localhost:8080/login
+
+### 2) Session 삭제
+
+* http://localhost:8080/logout
+
+### 3) Session 정보 확인
+
+* http://localhost:8080/info
---
+++ src/main/java/com/example/session/controller/SessionController.java
@@ -0,0 +1,38 @@
+package com.example.session.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class SessionController {
+
+    @PostMapping("/login")
+    public String login(HttpSession session) {
+
+        // 세션 생성
+        session.setMaxInactiveInterval(30 * 60); // timeout 30분 설정
+        session.setAttribute("id", "userid");
+        session.setAttribute("name", "username");
+        return session.getId();
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+
+        // 세션 삭제
+        session.invalidate();
+        return session.getId();
+    }
+
+    @PostMapping("/info")
+    public String info(HttpSession session) {
+
+        // 세션값 가져오기
+        Object id = session.getAttribute("id");
+        Object name = session.getAttribute("name");
+
+        return id.toString() + ", " + name.toString();
+    }
+}
